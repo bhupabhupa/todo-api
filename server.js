61aa83697312b6d4115130746e1f5eedc738d2eb
Bhupendra Tandel <bhupabhupa@gmail.com>
2018-04-04 23:44:56 +0530

    Put todo api

--- server.js
+++ server.js
@@ -13,12 +13,14 @@ app.get('/', function(req, res){
 	res.send('Todo api list');
 });
 
+
 //POST /todos
 app.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');
 	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(404).send();
 	}
+	
 	body.description = body.description.trim();
 	body.id = todoNextId;
 	todoNextId++;
@@ -48,6 +50,50 @@ app.get('/todos/:id', function(req, res){
 		res.status(404).send();		
 	}
 });
+
+//Delete /todos/:id
+app.delete('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchTodo = _.findWhere(todos, {id: todoId});
+
+	if(matchTodo) {
+		var todos = _.without(todos, matchTodo);	
+	} else {
+		res.status(404).json({"error": "no todo with that id"})
+	}
+	res.send(todos);
+})
+
+//Update //todos/:id
+app.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validateAttributes = {};
+
+	if(!matchTodo) {
+		res.status(404).json({"error": "no todo with that id"})
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validateAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).json({"error": "Wrong Completed"});
+	}
+
+	console.log(body.description);
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validateAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).json({"error": "Wrong Description"});
+	}
+	
+	_.extend(matchTodo, validateAttributes);
+	res.json(matchTodo);
+
+});
+
 
 app.listen(PORT, function(){
 	console.log('Express listing on port ' + PORT + '!');
